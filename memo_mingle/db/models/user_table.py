d4e34efec232be10fc41875b9a7a1e3fdbffeb4b
noa.dudai <dudai.noa@gmail.com>
2023-03-09 17:56:59 +0200

    features sign up, login and logout

--- memo_mingle/db/models/user_table.py
+++ memo_mingle/db/models/user_table.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from passlib.hash import scram
+from flask_login import UserMixin
 
 from memo_mingle.db.orm_config import Base
 from memo_mingle.db.models.summarys_table import SummaryModel
 
 # TODO: Add a login manager with Flask
 
-class UserModel(Base):
+class UserModel(Base, UserMixin):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, nullable=False)
